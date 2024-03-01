8fa2616e67a97c4d3e057fc4c78f6b5ec816e067
RustyNova <rusty.nova.jsb@gmail.com>
2024-03-01 10:53:12 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Listenbrainz-rs-utils
+
+A collection of utilities for the [listenbrain-rs](https://crates.io/crates/listenbrainz) crate. Those may or may not be included upstream later on.
+
+## Readers
+Readers allows easier handling of paginated APIs. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-//! This crate implements a few utilities for the [listenbrain-rs](https://crates.io/crates/listenbrainz) crate.
+//! # Listenbrainz-rs-utils
+//!
+//! A collection of utilities for the [listenbrain-rs](https://crates.io/crates/listenbrainz) crate. Those may or may not be included upstream later on.
+//!
+//! ## Readers
+//! Readers allows easier handling of paginated APIs. 
 
 #![deny(
     missing_docs,
